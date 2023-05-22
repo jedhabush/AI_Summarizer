04dc2a6d71f6f081e27a54a0b34d7d73135f4dd4
jed habush <jedhabush@jeds-MacBook-Air.local>
2023-05-22 11:07:33 +1000

    preparing redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,26 +2,35 @@ import { useState, useEffect } from "react"
 import {copy, linkIcon, loader, tick} from '../assets'
 
 const Demo = () => {
+const [article, setArticle] = useState({
+    url:"",
+    summary:""
+})
+
+const handleSubmit = async (e)=>{
+
+}
+
 return (
 <section className="mt-16 w-full max-w-xl">
 {/* search*/}
 <div className="flex flex-col w-full gap-2">
-<form action="" className="relative flex justify-center items-center" onSubmit={()=>{}}>
+<form action="" className="relative flex justify-center items-center" onSubmit={handleSubmit}>
 
 <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
 
 <input type="url" 
 placeholder="Enter URL" 
-value="" 
-onChange={()=>{}} 
+value={article.url}
+onChange={(e)=> setArticle({...article, url: e.target.value})} 
 required 
 className="url_input peer"
-    />
-    <button 
-    type="submit" 
-    className="submit_btn peer-focus:gray-700 peer-focus:text-700">
-    Enter
-    </button>
+/>
+<button 
+type="submit" 
+className="submit_btn peer-focus:gray-700 peer-focus:text-700">
+Enter
+</button>
 </form>
 {/* Browse URL history */}
 
